446d4400714a73649a245672f2feb75882da1bc8
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 19:24:01 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -36,34 +36,4 @@ describe 'collections practice' do
       expect(reverse_array([12, 4, 35])).to eq([35, 4, 12])
     end
   end
-
-  # Question 6
-  describe '#kesha_maker' do
-    # DO NOT USE .collect or .map. use .each to build a new array, like in the "my_each" lab!
-    it 'taking an array as an input, change the 3rd character of each element to a dollar sign.' do
-      expect(kesha_maker(["blake", "ashley", "scott"])).to eq(["bl$ke", "as$ley", "sc$tt"])
-    end
-  end
-
-  # Question 7
-  describe '#find_a' do
-    it 'find all words that begin with "a" in the following array' do
-      expect(find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ])).to eq(["apple", "avis", "arlo", "ascot"])
-    end
-  end
-
-  # Question 8
-  describe '#sum_array' do
-    #for a bonus, look up the `.inject` method to get this test passing
-    it 'sum all the numbers in the following array' do
-      expect(sum_array([11,4,7,8,9,100,134])).to eq(273)
-    end
-  end
-
-  # Question 9
-  describe '#add_s' do
-    it 'Add an "s" to each word in the array except for the 2nd element in the array' do
-      expect(add_s(["hand","feet", "knee", "table"])).to eq(["hands","feet", "knees", "tables"])
-    end
-  end
 end
